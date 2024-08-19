49ce35707db0f24690321e3152f8a4a2aa637c41
Mostafa Abdelmoneim <mostafa.abdelmoneim94@gmail.com>
2024-08-19 21:29:18 +0300

    Add project files.

---
+++ Strategy Pattern/Implmentation.cs
@@ -0,0 +1,68 @@
+﻿using StrategyPattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy_Pattern
+{
+
+    public interface IExportService
+    {
+        void Export(Order order);
+    }
+    public class CSVExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"CSV Exporting .....{order.Name}  , {order.Customer} , {order.Amount}");
+
+        }
+    }
+    public class PDFExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"PDF Exporting .....{order.Name}  , {order.Customer} , {order.Amount}");
+
+
+        }
+    }
+    public class XMLExportService : IExportService
+    {
+        public void Export(Order order)
+        {
+            Console.WriteLine($"XML Exporting .....{order.Name}  , {order.Customer} , {order.Amount}");
+
+        }
+    }
+    public class Order // context 
+    {
+        public int Id { get; set; }
+        public int Amount { get; set; }
+
+        public string Name { get; set; }
+
+        public string Customer { get; set; }
+        public string Description { get; set; }
+
+        // public IExportService? _exportService { get; set; }
+        public Order(int id, int amount, string name, string customer, string description)
+        {
+            Id = id;
+            Amount = amount;
+            Name = name;
+            Customer = customer;
+            Description = description;
+        }
+
+        public void Export(IExportService exportService)
+        {
+            if (exportService == null)
+                throw new NullReferenceException(nameof(exportService));
+            exportService?.Export(this);
+        }
+    }
+
+}
---
+++ Strategy Pattern/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Strategy_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order(1, 5, "Mobile", " Ahmed", "Samsung");
+            order.Export(new CSVExportService());
+
+            order.Export(new PDFExportService());
+
+            order.Export(new XMLExportService());
+
+            Console.ReadKey();
+        }
+    }
+}
