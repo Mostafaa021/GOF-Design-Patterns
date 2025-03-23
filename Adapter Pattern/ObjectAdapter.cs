a249630403b66746df4a8e026f6603fb32634cd0
Mostafa Abdelmoneim <mostafa.abdelmoneim94@gmail.com>
2025-03-23 10:43:04 +0200

    Add Object Adapter implementation for image resizing

--- Adapter Pattern/ObjectAdapter.cs
+++ Adapter Pattern/ObjectAdapter.cs
@@ -10,65 +10,65 @@
  // or in another meaning  it converts the interface of a class into another interface the client expects
  // it lets classes work together that couldn't otherwise because of incompatible interfaces
 
- // Library A's Image Resizer first option 
- public class LibraryAImageResizer  // adaptee
- {
-  public void Resize(int width, int height)
+  // Library A's Image Resizer first option 
+  public class LibraryAImageResizer  // adaptee
   {
-   Console.WriteLine($"Library A: Resized image to {width}x{height}");
+   public void Resize(int width, int height)
+   {
+    Console.WriteLine($"Library A: Resized image to {width}x{height}");
+   }
   }
- }
 
-// Library B's Image Resizer second option
- public class LibraryBImageResizer // adaptee
- {
-  public void  SetSize(int newWidth, int newHeight)
+ // Library B's Image Resizer second option
+  public class LibraryBImageResizer // adaptee
   {
-   Console.WriteLine($"Library B: Resized image to {newWidth}x{newHeight}");
+   public void  SetSize(int newWidth, int newHeight)
+   {
+    Console.WriteLine($"Library B: Resized image to {newWidth}x{newHeight}");
+   }
   }
- }
 
- // Target Interface 
- public interface IImageResizer // target
- {
-  void ResizeImage(int width, int height);
- }
-
- // Adapter Class for Library A 
- public class LibraryAImageResizerAdapter : IImageResizer // adapter
- {
-  private readonly LibraryAImageResizer _resizer = new();
-  public void ResizeImage(int width, int height)
+  // Target Interface 
+  public interface IImageResizer // target
   {
-   _resizer.Resize(width, height); // here the adaptation is done by converting the method name to match the client's expectation
+   void ResizeImage(int width, int height);
   }
- }
 
- // Adapter Class for Library B
- public class LibraryBImageResizerAdapter : IImageResizer // adapter
- {
-  private readonly LibraryBImageResizer _resizer = new();
-  public void ResizeImage(int width, int height)
+  // Adapter Class for Library A 
+  public class LibraryAImageResizerAdapter : IImageResizer // adapter
   {
-   _resizer.SetSize(width, height); // here the adaptation is done by converting the method name to match the client's expectation
+   private readonly LibraryAImageResizer _resizer = new();
+   public void ResizeImage(int width, int height)
+   {
+    _resizer.Resize(width, height); // here the adaptation is done by converting the method name to match the client's expectation
+   }
   }
- }
 
-
- // Client Code (directly using Library A)
- public class ImageProcessor
- {
-  private readonly IImageResizer _resizer ;
-
-  public ImageProcessor(IImageResizer resizer)
+  // Adapter Class for Library B
+  public class LibraryBImageResizerAdapter : IImageResizer // adapter
   {
-   _resizer = resizer; 
+   private readonly LibraryBImageResizer _resizer = new();
+   public void ResizeImage(int width, int height)
+   {
+    _resizer.SetSize(width, height); // here the adaptation is done by converting the method name to match the client's expectation
+   }
   }
-  public void ProcessImage(int width, int height)
+
+
+  // Client Code (directly using Library A)
+  public class ImageProcessor
   {
-   _resizer.ResizeImage(width, height); // here is abstraction of the client code to use the adapter
+   private readonly IImageResizer _resizer ;
+
+   public ImageProcessor(IImageResizer resizer)
+   {
+    _resizer = resizer; 
+   }
+   public void ProcessImage(int width, int height)
+   {
+    _resizer.ResizeImage(width, height); // here is abstraction of the client code to use the adapter
+   }
   }
- }
  
 
  
